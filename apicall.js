865ad86afc0f910550d0fc425253885fd8ad6784
MahiroReina <152889918+MahiroReina@users.noreply.github.com>
2025-09-16 17:30:25 +0700

    Update apicall.js

--- apicall.js
+++ apicall.js
@@ -1,12 +1,13 @@
 const main_url = `https://gelbooru.com`;
+const api_key = `&api_key=a6dca2045988ec286c39dff0f2b9448940a346b24cf25fe280f7d07bbb02d9fb&user_id=1171195`;
 
 const post_lst = `/index.php?page=dapi&q=index` + `&json=1`;
 const tag_lst = `/index.php?page=dapi&s=tag&q=index` + `&json=1`;
 
 const additional_tag = ``;
-const rating[4] = (general, 
+const rating[4] = [`general`, ] 
 
-fetch()
+fetch(main_url + post_lst + api_key)
   .then(response => {
     if (!response.ok)
       console.log(`Fetch data failed!`);
